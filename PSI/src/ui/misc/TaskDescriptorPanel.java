4e69fb97f95d9ed6f559432eecf735da97adc6ac
kasgoal <kasgoal>
2006-01-19 22:32:29 +0000

    *** empty log message ***

--- PSI/src/ui/misc/TaskDescriptorPanel.java
+++ PSI/src/ui/misc/TaskDescriptorPanel.java
@@ -2,7 +2,13 @@
 package ui.misc ;
 
 import javax.swing.JPanel ;
+
+import java.awt.Component ;
 import java.awt.Dimension ;
+import java.awt.GridBagConstraints ;
+import java.awt.GridBagLayout ;
+
+import javax.swing.Box ;
 import javax.swing.JScrollPane ;
 import javax.swing.JTable ;
 import javax.swing.table.AbstractTableModel ;
@@ -12,6 +18,7 @@ import model.spem2.TaskDescriptor ;
 import java.util.Locale ;
 import java.text.DateFormat ;
 import javax.swing.event.* ;
+import javax.swing.JLabel ;
 
 /**
  * JPanelTaskDescriptor : TODO type description
@@ -43,6 +50,26 @@ public class TaskDescriptorPanel extends JPanel
 
 	private DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale) ;
 
+	private JLabel idLabel = null ;
+
+	private JLabel idLabelValue = null ;
+
+	private JLabel nameLabel = null ;
+
+	private JLabel nameLabelValue = null ;
+
+	private JLabel descLabel = null ;
+
+	private JLabel descLabelValue = null ;
+
+	private JLabel decalLabel = null ;
+
+	private JPanel jPanel = null ;
+
+	private JPanel jPanelHaut = null ;
+
+	private TaskDescriptor task = null ;
+
 	/**
 	 * This is the default constructor
 	 */
@@ -50,15 +77,15 @@ public class TaskDescriptorPanel extends JPanel
 	{
 		super() ;
 		donnees = new Object[2][4] ;
-
+		task = _task ;
 		donnees[0][0] = Bundle.getText("JPanelTaskDescriptorRow1") ;
 		donnees[1][0] = Bundle.getText("JPanelTaskDescriptorRow2") ;
-		donnees[0][1] = dateFormat.format( (_task.getPlanningData()).getStartDate()) ;
-		donnees[1][1] = dateFormat.format( (_task.getRealData()).getStartDate()) ;
-		donnees[0][2] = dateFormat.format( (_task.getPlanningData()).getFinishDate()) ;
-		donnees[1][2] = dateFormat.format( (_task.getRealData()).getFinishDate()) ;
-		donnees[0][3] = (_task.getPlanningData()).getDuration() ;
-		donnees[1][3] = (_task.getRealData()).getDuration() ;
+		donnees[0][1] = dateFormat.format( (task.getPlanningData()).getStartDate()) ;
+		donnees[1][1] = dateFormat.format( (task.getRealData()).getStartDate()) ;
+		donnees[0][2] = dateFormat.format( (task.getPlanningData()).getFinishDate()) ;
+		donnees[1][2] = dateFormat.format( (task.getRealData()).getFinishDate()) ;
+		donnees[0][3] = (task.getPlanningData()).getDuration() ;
+		donnees[1][3] = (task.getRealData()).getDuration() ;
 
 		ModelTableTaskDescriptor tableModel = new ModelTableTaskDescriptor(donnees, nomsColonnes, _task) ;
 		initialize() ;
@@ -76,8 +103,60 @@ public class TaskDescriptorPanel extends JPanel
 	 */
 	private void initialize ()
 	{
+
 		this.setSize(300, 200) ;
 		setLayout(new java.awt.BorderLayout()) ;
+		this.add(getJPanel(), java.awt.BorderLayout.NORTH) ;
+	}
+
+	/**
+	 * This method initializes jPanel
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private JPanel getJPanel ()
+	{
+		GridBagConstraints c = new GridBagConstraints() ;
+
+		idLabel = new JLabel() ;
+		idLabelValue = new JLabel() ;
+		nameLabel = new JLabel() ;
+		nameLabelValue = new JLabel() ;
+		descLabel = new JLabel() ;
+		descLabelValue = new JLabel() ;
+		idLabel.setText(Bundle.getText("JPanelTaskDescriptorIDLabel")) ;
+		nameLabel.setText(Bundle.getText("JPanelTaskDescriptorNameLabel")) ;
+		descLabel.setText(Bundle.getText("JPanelTaskDescriptorDescriptionLabel")) ;
+		if (task.getId() != null) idLabelValue.setText(task.getId()) ;
+		if (task.getName() != null) nameLabelValue.setText(task.getName()) ;
+		if (task.getDescription() != null) descLabelValue.setText(task.getDescription()) ;
+		if (jPanel == null)
+		{
+			jPanel = new JPanel() ;
+			jPanel.setLayout(new GridBagLayout()) ;
+			c.gridx = 0 ;
+			c.gridy = 0 ;
+			c.gridwidth = 1 ;
+			c.gridheight = 1 ;
+			c.fill = GridBagConstraints.WEST ;
+			c.anchor = GridBagConstraints.WEST ;
+			jPanel.add(idLabel, c) ;
+			c.gridx = 1 ;
+			jPanel.add(idLabelValue, c) ;
+			c.gridx = 0 ;
+			c.gridy = 1 ;
+
+			jPanel.add(nameLabel, c) ;
+			c.gridx = 1 ;
+			jPanel.add(nameLabelValue, c) ;
+			c.gridx = 0 ;
+			c.gridy = 2 ;
+			jPanel.add(descLabel, c) ;
+			c.gridx = 1 ;
+			jPanel.add(descLabelValue, c) ;
+			jPanel.setVisible(true) ;
+		}
+		return jPanel ;
 	}
 
 }
@@ -90,6 +169,7 @@ public class TaskDescriptorPanel extends JPanel
  * @version 1.0
  * 
  */
+
 class ModelTableTaskDescriptor extends AbstractTableModel
 {
 	Object donnees[][] ;

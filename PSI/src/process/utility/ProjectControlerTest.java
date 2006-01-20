0f7c4f1be06accd22e3180fdfc1047c7fa2625c0
avego <avego>
2006-01-20 16:07:38 +0000

    Test of ProjectControl

---
+++ PSI/src/process/utility/ProjectControlerTest.java
@@ -0,0 +1,59 @@
+package process.utility;
+
+import java.io.File;
+
+import process.exception.FileParseException;
+
+import model.Project;
+
+import junit.framework.TestCase;
+
+/**
+ * ProjectControlerTest : TODO type description
+ *
+ * @author m1isi28
+ * @version 1.0
+ *
+ */
+public class ProjectControlerTest extends TestCase
+{
+
+	/*
+	 * Test method for 'process.utility.ProjectControler.create(File)'
+	 */
+	public void testCreate () throws FileParseException
+	{
+		File mauvais_fich = new File("/users/iupisi/m1isi28/BE/FICH/test.xml");
+		File bon_fich = new File("/users/iupisi/m1isi28/BE/FICH/open_workbench_project.xml");
+		Project pr1,pr2;
+		/**
+		 * project imported with the good format
+		 * to comment the next test for running this test
+		 */
+		pr1 = ProjectControler.create(bon_fich);
+		/**
+		 * project imported with the bad format
+		 */
+		pr2 = ProjectControler.create(mauvais_fich);
+		
+		
+		
+	}
+
+	/*
+	 * Test method for 'process.utility.ProjectControler.open(File)'
+	 */
+	public void testOpen ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.utility.ProjectControler.save(Project, File)'
+	 */
+	public void testSave ()
+	{
+
+	}
+
+}

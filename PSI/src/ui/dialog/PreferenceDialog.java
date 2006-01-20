0705f6cc771cb3d421f3c0b7c9a728d1832fabce
kasgoal <kasgoal>
2006-01-20 14:05:57 +0000

    *** empty log message ***

---
+++ PSI/src/ui/dialog/PreferenceDialog.java
@@ -0,0 +1,21 @@
+package ui.dialog;
+
+import javax.swing.JDialog;
+
+/**
+ * PreferenceDialog : TODO type description
+ *
+ * @author m1isi1
+ * @version 1.0
+ *
+ */
+public class PreferenceDialog
+{
+	private JDialog jDialog = null;
+	
+	
+	public JDialog getJDialog()
+	{
+		return jDialog;
+	}
+}

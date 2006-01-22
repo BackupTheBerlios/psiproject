b06b0f38772edb7f3adc6cc5511f7982d9180302
kasgoal <kasgoal>
2006-01-22 22:10:51 +0000

    *** empty log message ***

--- PSI/src/ui/dialog/PreferenceDialog.java
+++ PSI/src/ui/dialog/PreferenceDialog.java
@@ -1,270 +1,416 @@
-package ui.dialog;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+package ui.dialog ;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
+import java.awt.BorderLayout ;
+import java.awt.Component ;
+import java.awt.Dimension;
+import java.awt.GridLayout ;
+import java.util.Locale ;
 
-import ui.resource.Bundle;
+import javax.swing.DefaultListCellRenderer ;
+import javax.swing.JButton ;
+import javax.swing.JCheckBox ;
+import javax.swing.JComboBox ;
+import javax.swing.JDialog ;
+import javax.swing.JFrame ;
+import javax.swing.JLabel ;
+import javax.swing.JList ;
+import javax.swing.JPanel ;
+import javax.swing.JTabbedPane ;
+import javax.swing.JTextField ;
+import javax.swing.SwingUtilities ;
+import javax.swing.UIManager ;
+import javax.swing.UIManager.LookAndFeelInfo ;
+
+import process.Preferences ;
+import ui.window.MainFrame;
+import ui.resource.Bundle ;
+import ui.resource.LocaleController;
 
 /**
  * PreferenceDialog : TODO type description
- *
+ * 
  * @author m1isi1
  * @version 1.0
- *
+ * 
  */
-public class PreferenceDialog
+public class PreferenceDialog extends JDialog
 {
-	private JDialog jDialog = null;
-	private JTabbedPane jTabbedPaneOnglets = null;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2419928875825666824L ;
+
 	
-	
+	private JDialog jDialog = null ;
+
+	private JTabbedPane jTabbedPaneOnglets = null ;
+
+	private Preferences pref ;
+
 	// General Panel
-	private JPanel jPanelGeneral = null;
-	private JPanel jPanelGeneralChoixLangue = null;
-	private JPanel jPanelGeneralDefautLangage = null;
-	private JPanel jPanelGeneralLook = null;
-	private JLabel jLabelChoixLangue = null;
-	private JLabel jLabelDefautLangage = null;
-	private JLabel jLabelLook = null;
-	private JComboBox jComboBoxChoixLangue = null;
-	private JCheckBox jCheckBoxDefautLangage = null;
-	private JComboBox jComboBoxLook = null;
-	
+	private JPanel jPanelGeneral = null ;
+
+	private JPanel jPanelGeneralChoixLangue = null ;
+
+	private JPanel jPanelGeneralDefautLangage = null ;
+
+	private JPanel jPanelGeneralLook = null ;
+
+	private JLabel jLabelChoixLangue = null ;
+
+	private JLabel jLabelDefautLangage = null ;
+
+	private JLabel jLabelLook = null ;
+
+	private JComboBox jComboBoxChoixLangue = null ;
+
+	private JCheckBox jCheckBoxDefautLangage = null ;
+
+	private JComboBox jComboBoxLook = null ;
+
 	// Travail Panel
-	private JPanel jPanelTravail = null;
-	private JPanel jPanelTravailRepertoireTravail = null;
-	private JPanel jPanelTravailRepertoireExport = null;
-	private JPanel jPanelTravailDernierProjet = null;
-	private JPanel jPanelTravailAide = null;
-	private JLabel jLabelRepertoireTravail = null;
-	private JLabel jLabelRepertoireExport = null;
-	private JLabel jLabelDernierProjet = null;
-	private JLabel jLabelAide = null;
-	private JTextField jTextFieldRepertoireTravail = null;
-	private JButton jButtonRepertoireTravailChoisir = null;
-	private JTextField jTextFieldRepertoireExport = null;
-	private JButton jButtonRepertoireExportChoisir = null;
-	private JCheckBox jCheckBoxDernierProjet = null;
-	private JCheckBox jCheckBoxAide = null;
-	
+	private JPanel jPanelTravail = null ;
+
+	private JPanel jPanelTravailRepertoireTravail = null ;
+
+	private JPanel jPanelTravailRepertoireExport = null ;
+
+	private JPanel jPanelTravailDernierProjet = null ;
+
+	private JPanel jPanelTravailAide = null ;
+
+	private JLabel jLabelRepertoireTravail = null ;
+
+	private JLabel jLabelRepertoireExport = null ;
+
+	private JLabel jLabelDernierProjet = null ;
+
+	private JLabel jLabelAide = null ;
+
+	private JTextField jTextFieldRepertoireTravail = null ;
+
+	private JButton jButtonRepertoireTravailChoisir = null ;
+
+	private JTextField jTextFieldRepertoireExport = null ;
+
+	private JButton jButtonRepertoireExportChoisir = null ;
+
+	private JCheckBox jCheckBoxDernierProjet = null ;
+
+	private JCheckBox jCheckBoxAide = null ;
+
 	// Buttons OK And Cancel/Annuler
-	private JPanel jPanelValider = null;
-	private JButton jButtonPreferenceOK = null;
-	private JButton jButtonPreferenceCancel = null;
-	
-	
-	public JDialog getJDialog()
+	private JPanel jPanelValider = null ;
+
+	private JButton jButtonPreferenceOK = null ;
+
+	private JButton jButtonPreferenceCancel = null ;
+
+	@ SuppressWarnings ("deprecation")
+	public PreferenceDialog (JFrame parent)
 	{
-		if (jDialog == null)
-		{
-			jDialog = new JDialog();
-			jDialog.setLayout(new BorderLayout());
-			jDialog.add(getJTabbedPaneOnglets(), BorderLayout.CENTER);
-			jDialog.add(getJPanelValider(),BorderLayout.SOUTH);
-									
-		}
-		
-		return jDialog;
+		super(parent) ;
+		this.setLayout(new BorderLayout()) ;
+		// this.setVisible(true);
+		this.setSize(500, 300) ;
+		this.add(getJTabbedPaneOnglets(), BorderLayout.CENTER) ;
+		this.add(getJPanelValider(), BorderLayout.SOUTH) ;
+
+		show() ;
+		jDialog = this ;
 	}
-	
-	public JTabbedPane getJTabbedPaneOnglets()
+
+	public JTabbedPane getJTabbedPaneOnglets ()
 	{
 		if (jTabbedPaneOnglets == null)
 		{
-			jTabbedPaneOnglets = new JTabbedPane();
-			jTabbedPaneOnglets.addTab(Bundle.getText("JPanelPreferenceGeneralLabel"),getJPanelGeneral());
-			jTabbedPaneOnglets.addTab(Bundle.getText("JPanelPreferenceTravailLabel"),getJPanelTravail());
+			jTabbedPaneOnglets = new JTabbedPane() ;
+			jTabbedPaneOnglets.addTab(Bundle.getText("JPanelPreferenceGeneralLabel"), getJPanelGeneral()) ;
+			jTabbedPaneOnglets.addTab(Bundle.getText("JPanelPreferenceTravailLabel"), getJPanelTravail()) ;
 		}
-		
-		return jTabbedPaneOnglets;
+
+		jTabbedPaneOnglets.setVisible(true) ;
+		return jTabbedPaneOnglets ;
 	}
-	
+
 	// General methods
-	public JPanel getJPanelGeneral()
+	public JPanel getJPanelGeneral ()
 	{
 		if (jPanelGeneral == null)
 		{
-			jPanelGeneral = new JPanel();
-			jPanelGeneral.setLayout(new GridLayout(3,1));
-			
-			jPanelGeneral.add(getJPanelGeneralChoixLangue());
-			jPanelGeneral.add(getJPanelGeneralDefautLangage());
-			jPanelGeneral.add(getJPanelGeneralLook());
+			jPanelGeneral = new JPanel() ;
+			jPanelGeneral.setLayout(new GridLayout(3, 1)) ;
+
+			jPanelGeneral.add(getJPanelGeneralChoixLangue()) ;
+			jPanelGeneral.add(getJPanelGeneralDefautLangage()) ;
+			jPanelGeneral.add(getJPanelGeneralLook()) ;
 		}
-		
-		return jPanelGeneral;
+		jPanelGeneral.setVisible(true) ;
+		return jPanelGeneral ;
 	}
-	
-	public JPanel getJPanelGeneralChoixLangue()
+
+	public JPanel getJPanelGeneralChoixLangue ()
 	{
 		if (jPanelGeneralChoixLangue == null)
 		{
-			jPanelGeneralChoixLangue = new JPanel();
-			jLabelChoixLangue = new JLabel();
-			jLabelChoixLangue.setText(Bundle.getText("JPanelPreferenceGeneralLanguageLabel"));
-			jPanelGeneralChoixLangue.add(jLabelChoixLangue);
-			
-			jComboBoxChoixLangue = new JComboBox();
+			jPanelGeneralChoixLangue = new JPanel() ;
+			jLabelChoixLangue = new JLabel() ;
+			jLabelChoixLangue.setText(Bundle.getText("JPanelPreferenceGeneralLanguageLabel")) ;
+			jPanelGeneralChoixLangue.add(jLabelChoixLangue) ;
+
+			jComboBoxChoixLangue = new JComboBox() ;
 			jComboBoxChoixLangue.addItem(Bundle.getText("JPanelPreferenceGeneralLanguageEnglish"));
 			jComboBoxChoixLangue.addItem(Bundle.getText("JPanelPreferenceGeneralLanguageFrench"));
-			jPanelGeneralChoixLangue.add(jComboBoxChoixLangue);
+			
+			if (Locale.getDefault().toString().equals("fr_FR"))
+				
+				jComboBoxChoixLangue.setSelectedItem(Bundle.getText("JPanelPreferenceGeneralLanguageFrench")) ;
+			else
+				jComboBoxChoixLangue.setSelectedItem(Bundle.getText("JPanelPreferenceGeneralLanguageEnglish")) ;
+			
+			jPanelGeneralChoixLangue.add(jComboBoxChoixLangue) ;
 		}
-		
-		return jPanelGeneralChoixLangue;
+
+		jPanelGeneralChoixLangue.setVisible(true) ;
+		return jPanelGeneralChoixLangue ;
 	}
-	
-	public JPanel getJPanelGeneralDefautLangage()
+
+	public JPanel getJPanelGeneralDefautLangage ()
 	{
 		if (jPanelGeneralDefautLangage == null)
 		{
-			jPanelGeneralDefautLangage = new JPanel();	
-			jLabelDefautLangage = new JLabel();
-			jLabelDefautLangage.setText(Bundle.getText("JPanelPreferenceGeneralDefaultLanguageLabel"));
-			jCheckBoxDefautLangage = new JCheckBox();
-			
-			jPanelGeneralDefautLangage.add(jCheckBoxDefautLangage);
-			jPanelGeneralDefautLangage.add(jLabelDefautLangage);
-					
+			jPanelGeneralDefautLangage = new JPanel() ;
+			jLabelDefautLangage = new JLabel() ;
+			jLabelDefautLangage.setText(Bundle.getText("JPanelPreferenceGeneralDefaultLanguageLabel")) ;
+			jCheckBoxDefautLangage = new JCheckBox() ;
+
+			jPanelGeneralDefautLangage.add(jCheckBoxDefautLangage) ;
+			jPanelGeneralDefautLangage.add(jLabelDefautLangage) ;
+
 		}
-		
-		return jPanelGeneralDefautLangage;
+		jPanelGeneralDefautLangage.setVisible(true) ;
+		return jPanelGeneralDefautLangage ;
 	}
-	
-	public JPanel getJPanelGeneralLook()
+
+	public JPanel getJPanelGeneralLook ()
 	{
 		if (jPanelGeneralLook == null)
 		{
-			jPanelGeneralLook = new JPanel();	
-			jLabelLook = new JLabel();
-			jLabelLook.setText(Bundle.getText("JPanelPreferenceGeneralLookAndFeelLabel"));
-			jPanelGeneralLook.add(jLabelLook);
-			
-			jComboBoxLook = new JComboBox();
-			jPanelGeneralLook.add(jComboBoxLook);
-			
+			jPanelGeneralLook = new JPanel() ;
+			jLabelLook = new JLabel() ;
+			jLabelLook.setText(Bundle.getText("JPanelPreferenceGeneralLookAndFeelLabel")) ;
+			jPanelGeneralLook.add(jLabelLook) ;
+
+			jComboBoxLook = new JComboBox(UIManager.getInstalledLookAndFeels()) ;
+			jComboBoxLook.setRenderer(new lafInfoCellRenderer()) ;
+			jPanelGeneralLook.add(jComboBoxLook) ;
+
 		}
-		
-		return jPanelGeneralLook;
+		jPanelGeneralLook.setVisible(true) ;
+		return jPanelGeneralLook ;
 	}
+
 	// End General methods
-	
+
 	// Travail methods
-	public JPanel getJPanelTravail()
+	public JPanel getJPanelTravail ()
 	{
 		if (jPanelTravail == null)
 		{
-			jPanelTravail = new JPanel();
-			jPanelTravail.setLayout(new GridLayout(4,1));
-			
-			jPanelTravail.add(getJPanelTravailRepertoireTravail());
-			jPanelTravail.add(getJPanelTravailRepertoireExport());
-			jPanelTravail.add(getJPanelTravailDernierProjet());
-			jPanelTravail.add(getJPanelTravailAide());
+			jPanelTravail = new JPanel() ;
+			jPanelTravail.setLayout(new GridLayout(4, 1)) ;
+
+			jPanelTravail.add(getJPanelTravailRepertoireTravail()) ;
+			jPanelTravail.add(getJPanelTravailRepertoireExport()) ;
+			jPanelTravail.add(getJPanelTravailDernierProjet()) ;
+			jPanelTravail.add(getJPanelTravailAide()) ;
 		}
-		
-		return jPanelTravail;
+		jPanelTravail.setVisible(true) ;
+		return jPanelTravail ;
 	}
-	
-	public JPanel getJPanelTravailRepertoireTravail()
+
+	public JPanel getJPanelTravailRepertoireTravail ()
 	{
 		if (jPanelTravailRepertoireTravail == null)
 		{
-			jPanelTravailRepertoireTravail = new JPanel();
-			jLabelRepertoireTravail = new JLabel();
-			jLabelRepertoireTravail.setText(Bundle.getText("JPanelPreferenceTravailWorkRepertoryLabel"));
-			jPanelTravailRepertoireTravail.add(jLabelRepertoireTravail);
+			jPanelTravailRepertoireTravail = new JPanel() ;
+			jLabelRepertoireTravail = new JLabel() ;
+			jLabelRepertoireTravail.setText(Bundle.getText("JPanelPreferenceTravailWorkRepertoryLabel")) ;
+			jPanelTravailRepertoireTravail.add(jLabelRepertoireTravail) ;
+
+			jTextFieldRepertoireTravail = new JTextField() ;
+			jTextFieldRepertoireTravail.setPreferredSize(new Dimension(100,20));
+			jPanelTravailRepertoireTravail.add(jTextFieldRepertoireTravail) ;
 			
-			jTextFieldRepertoireTravail = new JTextField();
-			jPanelTravailRepertoireTravail.add(jTextFieldRepertoireTravail);
-			
-			jButtonRepertoireTravailChoisir = new JButton();
-			jButtonRepertoireTravailChoisir.setText(Bundle.getText("JPanelPreferenceTravailParcourirButton"));
-			jPanelTravailRepertoireTravail.add(jButtonRepertoireTravailChoisir);
+
+			jButtonRepertoireTravailChoisir = new JButton() ;
+			jButtonRepertoireTravailChoisir.setText(Bundle.getText("JPanelPreferenceTravailParcourirButton")) ;
+			jButtonRepertoireTravailChoisir.setPreferredSize(new Dimension(100,20));
+			jPanelTravailRepertoireTravail.add(jButtonRepertoireTravailChoisir) ;
 		}
-		
-		return jPanelTravailRepertoireTravail;
+		jPanelTravailRepertoireTravail.setVisible(true) ;
+		return jPanelTravailRepertoireTravail ;
 	}
-	
-	public JPanel getJPanelTravailRepertoireExport()
+
+	public JPanel getJPanelTravailRepertoireExport ()
 	{
 		if (jPanelTravailRepertoireExport == null)
 		{
-			jPanelTravailRepertoireExport = new JPanel();
-			jLabelRepertoireExport = new JLabel();
-			jLabelRepertoireExport.setText(Bundle.getText("JPanelPreferenceTravailExportRepertoryLabel"));
-			jPanelTravailRepertoireExport.add(jLabelRepertoireExport);
+			jPanelTravailRepertoireExport = new JPanel() ;
+			jLabelRepertoireExport = new JLabel() ;
+			jLabelRepertoireExport.setText(Bundle.getText("JPanelPreferenceTravailExportRepertoryLabel")) ;
+			jPanelTravailRepertoireExport.add(jLabelRepertoireExport) ;
+
+			jTextFieldRepertoireExport = new JTextField() ;
+			jTextFieldRepertoireExport.setPreferredSize(new Dimension(100,20));
+			jPanelTravailRepertoireExport.add(jTextFieldRepertoireExport) ;
 			
-			jTextFieldRepertoireExport = new JTextField();
-			jPanelTravailRepertoireExport.add(jTextFieldRepertoireExport);
 			
-			jButtonRepertoireExportChoisir = new JButton();
-			jButtonRepertoireExportChoisir.setText(Bundle.getText("JPanelPreferenceTravailParcourirButton"));
-			jPanelTravailRepertoireExport.add(jButtonRepertoireExportChoisir);
+
+			jButtonRepertoireExportChoisir = new JButton() ;
+			jButtonRepertoireExportChoisir.setText(Bundle.getText("JPanelPreferenceTravailParcourirButton")) ;
+			jButtonRepertoireExportChoisir.setPreferredSize(new Dimension(100,20));
+			jPanelTravailRepertoireExport.add(jButtonRepertoireExportChoisir) ;
 		}
-		
-		return jPanelTravailRepertoireExport;
+		jPanelTravailRepertoireExport.setVisible(true) ;
+		return jPanelTravailRepertoireExport ;
 	}
 
-	public JPanel getJPanelTravailDernierProjet()
+	public JPanel getJPanelTravailDernierProjet ()
 	{
 		if (jPanelTravailDernierProjet == null)
 		{
-			jPanelTravailDernierProjet = new JPanel();
-			jLabelDernierProjet = new JLabel();
-			jLabelDernierProjet.setText(Bundle.getText("JPanelPreferenceTravailLastProjectLabel"));
-			jCheckBoxDernierProjet = new JCheckBox();
-			
-			jPanelTravailDernierProjet.add(jCheckBoxDernierProjet);
-			jPanelTravailDernierProjet.add(jLabelDernierProjet);
-						
+			jPanelTravailDernierProjet = new JPanel() ;
+			jLabelDernierProjet = new JLabel() ;
+			jLabelDernierProjet.setText(Bundle.getText("JPanelPreferenceTravailLastProjectLabel")) ;
+			jCheckBoxDernierProjet = new JCheckBox() ;
+
+			jPanelTravailDernierProjet.add(jCheckBoxDernierProjet) ;
+			jPanelTravailDernierProjet.add(jLabelDernierProjet) ;
+
 		}
-		
-		return jPanelTravailRepertoireExport;
+		jPanelTravailRepertoireExport.setVisible(true) ;
+		return jPanelTravailRepertoireExport ;
 	}
-	
-	public JPanel getJPanelTravailAide()
+
+	public JPanel getJPanelTravailAide ()
 	{
-		if (jPanelTravailAide== null)
+		if (jPanelTravailAide == null)
 		{
-			jPanelTravailAide = new JPanel();
-			jLabelAide = new JLabel();
-			jLabelAide.setText(Bundle.getText("JPanelPreferenceTravailHelpLabel"));
-			jCheckBoxAide = new JCheckBox();
-			
-			jPanelTravailAide.add(jCheckBoxAide);
-			jPanelTravailAide.add(jLabelAide);
-						
+			jPanelTravailAide = new JPanel() ;
+			jLabelAide = new JLabel() ;
+			jLabelAide.setText(Bundle.getText("JPanelPreferenceTravailHelpLabel")) ;
+			jCheckBoxAide = new JCheckBox() ;
+
+			jPanelTravailAide.add(jCheckBoxAide) ;
+			jPanelTravailAide.add(jLabelAide) ;
+
 		}
-		
-		return jPanelTravailRepertoireExport;
+		jPanelTravailRepertoireExport.setVisible(true) ;
+		return jPanelTravailRepertoireExport ;
 	}
-	
-	//	End Travail methods
-	
-	
-	public JPanel getJPanelValider()
+
+	// End Travail methods
+
+	public JPanel getJPanelValider ()
 	{
 		if (jPanelValider == null)
 		{
-			jPanelValider = new JPanel();
-			jButtonPreferenceOK = new JButton();
-			jButtonPreferenceCancel = new JButton();
-			jButtonPreferenceOK.setText(Bundle.getText("JPanelValiderOKLabel"));
-			jButtonPreferenceCancel.setText(Bundle.getText("JPanelValiderCancelLabel"));
-			jPanelValider.add(jButtonPreferenceOK);
-			jPanelValider.add(jButtonPreferenceCancel);
+			jPanelValider = new JPanel() ;
+			jButtonPreferenceOK = new JButton() ;
+			jButtonPreferenceCancel = new JButton() ;
+			jButtonPreferenceOK.setText(Bundle.getText("JPanelValiderOKLabel")) ;
+			jButtonPreferenceCancel.setText(Bundle.getText("JPanelValiderCancelLabel")) ;
+			jPanelValider.add(jButtonPreferenceOK) ;
+			jPanelValider.add(jButtonPreferenceCancel) ;
+
+			jButtonPreferenceOK.addActionListener(new java.awt.event.ActionListener()
+			{
+				public void actionPerformed (java.awt.event.ActionEvent e)
+				{
+
+					// TRAITEMENT DU LOOK AND FELL
+					LookAndFeelInfo tempLAFInfo = (LookAndFeelInfo) (jComboBoxLook.getSelectedItem()) ;
+					try
+					{
+						
+						UIManager.setLookAndFeel(tempLAFInfo.getClassName()) ;
+						SwingUtilities.updateComponentTreeUI(jDialog) ;
+						SwingUtilities.updateComponentTreeUI(jDialog.getParent()) ;
+					}
+					catch (Exception exc)
+					{
+						exc.printStackTrace() ;
+					}
+					
+					// TRAITEMENT DE LA LANGUE
+					//On regarde la langue choisie et on change la Locale
+			        if(jComboBoxChoixLangue.getSelectedItem().toString().compareTo(Bundle.getText("JPanelPreferenceGeneralLanguageFrench")) == 0)
+			        {
+			        	
+			        	Bundle.setCurrentLocale(Locale.FRENCH);
+			        	//MainFrame.ControllerLocale.fireLocaleChanged();
+			        	if (jCheckBoxDefautLangage.isSelected())
+			        	{
+			        		//MainFrame.localPrefs.setLocale("FR");
+			        	}
+			        		
+			        	
+			        }
+			        else if(jComboBoxChoixLangue.getSelectedItem().toString().compareTo(Bundle.getText("JPanelPreferenceGeneralLanguageEnglish")) == 0)
+			        {
+			        	
+			        	Bundle.setCurrentLocale(Locale.ENGLISH);
+			        	//MainFrame.ControllerLocale.fireLocaleChanged();
+			        	if (jCheckBoxDefautLangage.isSelected())
+			        	{
+			        		
+			        		//MainFrame.localPrefs.setLocale("EN");
+			        		
+			        	}
+			            
+			        }
+			       
+					dispose() ;
+
+				}
+			}) ;
+
+			jButtonPreferenceCancel.addActionListener(new java.awt.event.ActionListener()
+			{
+				public void actionPerformed (java.awt.event.ActionEvent e)
+				{
+					dispose() ;
+				}
+			}) ;
+
 		}
-		
-		return jPanelValider;
-	}	
-	
+		jPanelValider.setVisible(true) ;
+		return jPanelValider ;
+	}
+
+}
+
+
+class lafInfoCellRenderer extends DefaultListCellRenderer
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L ;
+
+	public lafInfoCellRenderer ()
+	{
+
+	}
+
+	public Component getListCellRendererComponent (JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+	{
+		Component comp = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus) ;
+		JLabel lafComp = (JLabel) comp ;
+		LookAndFeelInfo lafInfo = (LookAndFeelInfo) value ;
+		lafComp.setText(lafInfo.getName()) ;
+		return lafComp ;
+	}
 }

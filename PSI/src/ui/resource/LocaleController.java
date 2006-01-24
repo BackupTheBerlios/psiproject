32378ca83a903e745b2a0c48a3e0a653ac050c65
kasgoal <kasgoal>
2006-01-24 21:22:36 +0000

    *** empty log message ***

--- PSI/src/ui/resource/LocaleController.java
+++ PSI/src/ui/resource/LocaleController.java
@@ -1,58 +1,85 @@
-package ui.resource;
 
-import java.util.Locale;
+package ui.resource ;
 
-import javax.swing.event.EventListenerList;
+import java.util.Locale ;
+
+import javax.swing.event.EventListenerList ;
+
+import process.Preferences ;
 
 /**
  * LocalController : TODO type description
- *
+ * 
  * @author Kassem
  * @version 1.0
- *
+ * 
  */
 public class LocaleController
 {
 	/**
-     * Liste contenant tous les LocaleListener
-     */    
-    protected EventListenerList listenerList = new EventListenerList(); 
-    
-    /**
-     * Ajoute un LocaleListener
-     * @param l LocaleListener a ajouter a la liste
-     */    
-    public void addLocaleListener (LocaleListener l) { 
-        listenerList.add(LocaleListener.class, l); 
-    } 
-    
-    /**
-     * Supprime un LocaleListener
-     * @param l LocaleListener a supprimer
-     */    
-    public void removeLocaleListener (LocaleListener l) { 
-        listenerList.remove(LocaleListener.class, l); 
-    } 
-    
-    /**
-     * Change la langue de tous les LocaleListener enregistres
-     */    
-    public void fireLocaleChanged () { 
-        LocaleListener[] listeners = (LocaleListener[]) 
-            listenerList.getListeners(LocaleListener.class); 
-       
-        for (int i = 0; i < listeners.length; i++) { 
-            listeners[i].localeChanged(); 
-        } 
-    } 
-    
-    /**
-     * Change la locale et appelle fireLocaleChanged()
-     * @param newLocale Nouvelle Locale
-     */    
-    public void changeLocale (Locale newLocale) { 
-        Locale.setDefault(newLocale); 
-        Bundle.setCurrentLocale(newLocale);
-        fireLocaleChanged(); 
-    }
+	 * Liste contenant tous les LocaleListener
+	 */
+
+	private static LocaleController instance = null ;
+
+	protected EventListenerList listenerList = new EventListenerList() ;
+
+	/**
+	 * Ajoute un LocaleListener
+	 * 
+	 * @param l
+	 *            LocaleListener a ajouter a la liste
+	 */
+	public void addLocaleListener (LocaleListener l)
+	{
+		listenerList.add(LocaleListener.class, l) ;
+	}
+
+	/**
+	 * Supprime un LocaleListener
+	 * 
+	 * @param l
+	 *            LocaleListener a supprimer
+	 */
+	public void removeLocaleListener (LocaleListener l)
+	{
+		listenerList.remove(LocaleListener.class, l) ;
+	}
+
+	/**
+	 * Change la langue de tous les LocaleListener enregistres
+	 */
+	public void fireLocaleChanged ()
+	{
+		LocaleListener[] listeners = (LocaleListener[]) listenerList.getListeners(LocaleListener.class) ;
+
+		for (int i = 0; i < listeners.length; i++ )
+		{
+			listeners[i].localeChanged() ;
+		}
+	}
+
+	/**
+	 * Change la locale et appelle fireLocaleChanged()
+	 * 
+	 * @param newLocale
+	 *            Nouvelle Locale
+	 */
+	public void changeLocale (Locale newLocale)
+	{
+		Locale.setDefault(newLocale) ;
+		Bundle.setCurrentLocale(newLocale) ;
+		fireLocaleChanged() ;
+	}
+
+	public static LocaleController getInstance ()
+	{
+		if (instance == null)
+		{
+			instance = new LocaleController() ;
+
+		}
+
+		return instance ;
+	}
 }

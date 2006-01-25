13c3740c9303c0e4e0de03086cebfb3254757d0b
avego <avego>
2006-01-25 12:34:02 +0000

    *** empty log message ***

---
+++ PSI/src/junit/ui/dialog/PreferenceDialogTest.java
@@ -0,0 +1,15 @@
+package junit.ui.dialog;
+
+import junit.framework.TestCase;
+
+/**
+ * PreferenceDialogTest : TODO type description
+ *
+ * @author m1isi28
+ * @version 1.0
+ *
+ */
+public class PreferenceDialogTest extends TestCase
+{
+
+}

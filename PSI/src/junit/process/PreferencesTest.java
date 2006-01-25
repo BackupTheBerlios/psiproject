14079217cf9ea1e90447e2123ec459784d75c544
avego <avego>
2006-01-25 12:29:28 +0000

    Test of classes from process package

---
+++ PSI/src/junit/process/PreferencesTest.java
@@ -0,0 +1,239 @@
+package junit.process;
+
+import junit.framework.TestCase;
+
+/**
+ * PreferencesTest : TODO type description
+ *
+ * @author m1isi28
+ * @version 1.0
+ *
+ */
+public class PreferencesTest extends TestCase
+{
+
+	/*
+	 * Test method for 'process.Preferences.save()'
+	 */
+	public void testSave ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getHeight()'
+	 */
+	public void testGetHeight ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setHeight(int)'
+	 */
+	public void testSetHeight ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLastProject()'
+	 */
+	public void testGetLastProject ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLastProject(String)'
+	 */
+	public void testSetLastProject ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getTreeWidth()'
+	 */
+	public void testGetTreeWidth ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setTreeWidth(int)'
+	 */
+	public void testSetTreeWidth ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLocale()'
+	 */
+	public void testGetLocale ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLocale(String)'
+	 */
+	public void testSetLocale ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLogHeight()'
+	 */
+	public void testGetLogHeight ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLogHeight(int)'
+	 */
+	public void testSetLogHeight ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getWidth()'
+	 */
+	public void testGetWidth ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setWidth(int)'
+	 */
+	public void testSetWidth ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getXPosition()'
+	 */
+	public void testGetXPosition ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setXPosition(int)'
+	 */
+	public void testSetXPosition ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getYPosition()'
+	 */
+	public void testGetYPosition ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setYPosition(int)'
+	 */
+	public void testSetYPosition ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getInstance()'
+	 */
+	public void testGetInstance ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getExportDirectory()'
+	 */
+	public void testGetExportDirectory ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setExportDirectory(String)'
+	 */
+	public void testSetExportDirectory ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLoadHelp()'
+	 */
+	public void testGetLoadHelp ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLoadHelp(boolean)'
+	 */
+	public void testSetLoadHelp ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLoadLastProject()'
+	 */
+	public void testGetLoadLastProject ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLoadLastProject(boolean)'
+	 */
+	public void testSetLoadLastProject ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getLookAndFeel()'
+	 */
+	public void testGetLookAndFeel ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setLookAndFeel(String)'
+	 */
+	public void testSetLookAndFeel ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.getWorkDirectory()'
+	 */
+	public void testGetWorkDirectory ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'process.Preferences.setWorkDirectory(String)'
+	 */
+	public void testSetWorkDirectory ()
+	{
+
+	}
+
+}

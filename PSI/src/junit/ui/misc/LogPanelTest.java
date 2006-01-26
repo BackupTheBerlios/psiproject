219edbf994bcc0aeeb244d0aac865902599e6b6d
avego <avego>
2006-01-26 05:26:10 +0000

    tests of classes from ui.misc package

---
+++ PSI/src/junit/ui/misc/LogPanelTest.java
@@ -0,0 +1,15 @@
+package junit.ui.misc;
+
+import junit.framework.TestCase;
+
+/**
+ * LogPanelTest : TODO type description
+ *
+ * @author Chez Maggy
+ * @version 1.0
+ *
+ */
+public class LogPanelTest extends TestCase
+{
+
+}

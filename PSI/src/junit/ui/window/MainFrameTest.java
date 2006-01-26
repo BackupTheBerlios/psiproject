341ccb7de3f04b0131b257c49baa9deab9a3610c
avego <avego>
2006-01-26 05:30:47 +0000

    Tests of MainFrame

---
+++ PSI/src/junit/ui/window/MainFrameTest.java
@@ -0,0 +1,39 @@
+package junit.ui.window;
+
+import junit.framework.TestCase;
+
+/**
+ * MainFrameTest : TODO type description
+ *
+ * @author Chez Maggy
+ * @version 1.0
+ *
+ */
+public class MainFrameTest extends TestCase
+{
+
+	/*
+	 * Test method for 'ui.window.MainFrame.MainFrame()'
+	 */
+	public void testMainFrame ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.window.MainFrame.getCurrentProject()'
+	 */
+	public void testGetCurrentProject ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.window.MainFrame.getProject()'
+	 */
+	public void testGetProject ()
+	{
+
+	}
+
+}

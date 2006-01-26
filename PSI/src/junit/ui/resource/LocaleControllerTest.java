16b21315ab6db5dc0fcdcb92aece56077bf10ac0
avego <avego>
2006-01-26 05:27:45 +0000

    Tests of classes from ui.resource

---
+++ PSI/src/junit/ui/resource/LocaleControllerTest.java
@@ -0,0 +1,55 @@
+package junit.ui.resource;
+
+import junit.framework.TestCase;
+
+/**
+ * LocaleControllerTest : TODO type description
+ *
+ * @author Chez Maggy
+ * @version 1.0
+ *
+ */
+public class LocaleControllerTest extends TestCase
+{
+
+	/*
+	 * Test method for 'ui.resource.LocaleController.addLocaleListener(LocaleListener)'
+	 */
+	public void testAddLocaleListener ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.resource.LocaleController.removeLocaleListener(LocaleListener)'
+	 */
+	public void testRemoveLocaleListener ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.resource.LocaleController.fireLocaleChanged()'
+	 */
+	public void testFireLocaleChanged ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.resource.LocaleController.changeLocale(Locale)'
+	 */
+	public void testChangeLocale ()
+	{
+
+	}
+
+	/*
+	 * Test method for 'ui.resource.LocaleController.getInstance()'
+	 */
+	public void testGetInstance ()
+	{
+
+	}
+
+}

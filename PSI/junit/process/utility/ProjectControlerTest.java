4e03b117034ac2b941fde49b372db716cc2ef644
hasun <hasun>
2006-02-03 15:03:02 +0000

    tyty

--- PSI/junit/process/utility/ProjectControlerTest.java
+++ PSI/junit/process/utility/ProjectControlerTest.java
@@ -3,6 +3,7 @@ package junit.process.utility;
 import java.io.File;
 
 import process.exception.FileParseException;
+import process.exception.FileSaveException;
 import process.utility.ProjectControler;
 
 import model.Project;
@@ -12,7 +13,7 @@ import junit.framework.TestCase;
 /**
  * ProjectControlerTest : Test different functions of ProjectControl
  *
- * @author Avetisyan Gohar
+ * @author Avetisyan Gohar & KOUCH Hassan
  * @version 1.0
  *
  */
@@ -25,20 +26,31 @@ public class ProjectControlerTest extends TestCase
 	 */
 	public void testCreate (File fich) throws FileParseException
 	{
-		try{
+		File ficPasBon = new File("/users/iupisi/l3isi21/Test.xml");
+		File ficBon = new File("/users/iupisi/l3isi21/open_workbench_project.xml");
+	
+		Project pr1 = ProjectControler.create(ficBon);
+		Project pr2 = ProjectControler.create(ficPasBon);	
+	
+	 }	/*	try{
 			projet = ProjectControler.create(fich);
 		}
 		catch(FileParseException fpe){
 			testOk = 0;	
 		}
-	}
+	}*/
 
 	/*
 	 * Test method for 'process.utility.ProjectControler.open(File)'
 	 */
 	public void testOpen ()
 	{
-
+		File ficPasBon = new File("/users/iupisi/l3isi21/Test.xml");
+		File ficBon = new File("/users/iupisi/l3isi21/open_workbench_project.xml");
+	
+		Project pr1 = ProjectControler.open(ficBon);
+		Project pr2 = ProjectControler.open(ficPasBon);
+	
 	}
 
 	/*
@@ -46,7 +58,36 @@ public class ProjectControlerTest extends TestCase
 	 */
 	public void testSave ()
 	{
+		
+	File ficPasBon = new File("/users/iupisi/l3isi21/Test.xml");
+	File ficBon = new File("/users/iupisi/l3isi21/open_workbench_project.xml");
 
+	Project pr1 = null;
+	Project pr2 = null;
+	
+	try
+	{
+		ProjectControler.save(pr1,ficBon);
 	}
+	catch (FileSaveException exc)
+	{
+		// TODO Auto-generated catch block
+		exc.printStackTrace();
+	}
+	try
+	{
+		ProjectControler.save(pr2,ficPasBon);
+	}
+	catch (FileSaveException exc)
+	{
+		int d
+		
+		// TODO Auto-generated catch block
+		exc.printStackTrace();
+	}
+	
+}
+
+	
 }
 
